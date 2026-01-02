cd04c70ee89615a4e5ae743f4d0ff415a969a63e
mohamex2342 <163596873+mohamex2342@users.noreply.github.com>
2026-01-02 13:30:57 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// مثال: دالة عامة لإرسال طلبات إلى الخادم
-async function sendRequest(endpoint, data) {
-    const response = await fetch(endpoint, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-    });
-    return response.json();
-}
